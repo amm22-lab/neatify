0711abfc3e0b63a803a0859dabff34f5bcf60ab1
Alex M <amm22@williams.edu>
2026-02-08 08:11:55 -0500

    Added annual folder

--- neatify.py
+++ neatify.py
@@ -1,5 +1,6 @@
 import shutil
 from pathlib import Path
+from datetime import datetime
 
 def organize_downloads():
     downloads_path = Path.home() / "Downloads"
@@ -13,16 +14,27 @@ def organize_downloads():
         "Icons": [".icns"]
     }
 
-    for folder in files_categories:
-        (downloads_path / folder).mkdir(exist_ok=True)
+    # for folder in files_categories:
+    #     (downloads_path / folder).mkdir(exist_ok=True)
+
     for file_path in downloads_path.iterdir():
         if file_path.is_dir() or file_path.name == "organizer.py":
             continue
+
         extension = file_path.suffix.lower()
+
         for folder in files_categories:
             if extension in files_categories[folder]:
-                destination = downloads_path / folder / file_path.name
-                print(f"Moving: {file_path.name} -> {folder}")
+                stat = file_path.stat()
+                print(stat)
+                year = datetime.fromtimestamp(stat.st_birthtime).year
+
+                destination_folder = downloads_path / str(year) / folder
+                destination_folder.mkdir(parents=True, exist_ok=True)
+
+                destination = destination_folder / file_path.name
+
+                print(f"Moving: {file_path.name} -> {year}/{folder}")
                 shutil.move(file_path, destination)
                 break
 
